5f69a9f31d41ea28a141dab306bb3e3aa47496c9
saha,haimanti <haimanti.saha17@gmail.com>
2020-07-12 22:07:50 -0400

    return keyword

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,14 @@
 blockchain=[[1]]
 
-
+def get_value_blockchain():
+    return blockchain [-1]
 
 def add_value(transaction_amount):
-    blockchain.append([blockchain[-1],transaction_amount])
-    print(blockchain)
+    blockchain.append([get_value_blockchain(),transaction_amount])
+    
 
 add_value(2)
 add_value(34)
 add_value(86)
 
+print(blockchain)
