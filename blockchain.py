e11a94714066dd64406b25a1fb08cadd42cb4516
saha,haimanti <haimanti.saha17@gmail.com>
2020-07-12 22:50:26 -0400

    passing argument

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,14 @@
-blockchain=[[1]]
+blockchain=[]
 
 def get_value_blockchain():
     return blockchain [-1]
 
-def add_value(transaction_amount):
-    blockchain.append([get_value_blockchain(),transaction_amount])
+def add_value(transaction_amount,last_transaction=[1]):
+    blockchain.append([last_transaction,transaction_amount])
     
 
 add_value(2)
-add_value(34)
-add_value(86)
+add_value(34,get_value_blockchain())
+add_value(86,get_value_blockchain())
 
 print(blockchain)
